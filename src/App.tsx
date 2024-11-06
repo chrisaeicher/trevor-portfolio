f418a6d0e7ffdd377374a8eb2ab7763f3574a577
Chris Eicher <chrisaeicher@gmail.com>
2024-11-05 22:34:08 -0800

    added portfolio image, resume, favicon. added background gradient and center content block.

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,69 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import TREVOR from './images/trevor.jpg';
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const certs = [
+    {
+      name: 'CompTIA PenTest+',
+      date: '5/22/24'
+    },
+    {
+      name: 'CompTIA Network+',
+      date: '6/30/22'
+    },
+    {
+      name: 'CompTIA CySA+',
+      date: '12/14/23'
+    },
+    {
+      name: 'CompTIA A+',
+      date: '11/30/21'
+    },
+    {
+      name: 'CompTIA Security+',
+      date: '10/27/22'
+    },
+    {
+      name: 'CompTIA Project+',
+      date: '5/18/22'
+    },
+    {
+      name: 'Associate of ISC2',
+      date: '9/5/23'
+    },
+    {
+      name: 'CompTIA Network Vulnerability Assessment Professional',
+      date: '5/22/24'
+    },
+    {
+      name: 'CompTIA IT Operations Specialist',
+      date: '6/30/22'
+    },
+    {
+      name: 'CompTIA Security Analytics Professional',
+      date: '12/14/23'
+    },
+    {
+      name: 'CompTIA Network Security Professional',
+      date: '5/22/24'
+    },
+  ];
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='App flex justify-center'>
+      <main className='flex flex-col py-24 items-center h-screen w-5/6 sm:w-3/4 md:w-2/3 max-w-[2556px] bg-zinc-900 shadow-black shadow-2xl'>
+        <div className='flex flex-col md:flex-row w-1/2'>
+          <div className='flex flex-col'>
+            <span className='text-green-tn text-3xl'>Hi, I'm</span>
+            <h1 className='text-4xl text-white'>Trevor Nuckles</h1>
+            <p className='mt-8'>I'm a skilled IT professional with a Bachelor of Science in Information Technology and experience in programming, systems management, and team leadership.</p>
+          </div>
+          <img className='w-64 h-64 rounded-full' src={TREVOR} alt='Trevor Nuckles' />
+        </div>
+      </main>
+    </div>
   )
 }
+
+  
 
 export default App
--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: { 
+      colors: {
+        'green': {
+          'tn': '#00ff8b',
+        }
+      },
+    },
   },
   plugins: [],
 }
